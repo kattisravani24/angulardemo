9385ea46d05a9e076e734fc03c44a14be91bba03
kattisravni24 <kattisravni24@gmail.com>
2020-04-15 22:48:40 +0530

    latest commits

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -14,8 +14,24 @@ export class MoviesComponent implements OnInit {
   }
   onAddingMovie(){
     this.movieName = (document.getElementById("addMovie") as HTMLInputElement).value;
-    console.log(this.movieName);
-    this.newMovie = this.movieList.push(this.movieName);
+    // console.log(this.movieName);
+    if(this.movieName === ''){
+      alert('Enter Movie Name');
+    }
+    else
+    {
+      this.newMovie = this.movieList.push(this.movieName);
+    }
     (document.getElementById("addMovie") as HTMLInputElement).value = '';
+  }
+  onDeleteMovie(i){
+    console.log(this.movieList.length-1);
+    this.movieList.splice(i, 1)
+  }
+  onSearchMovie(){
+    this.movieName = (document.getElementById("searchMovie") as HTMLInputElement).value;
+    if(this.movieName === ''){
+      alert('Enter Movie Name');
+    }
   }
 }
--- src/app/myPipes/reverseArr.ts
+++ src/app/myPipes/reverseArr.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'reverseArray'})
 export class ReverseArray implements PipeTransform{
-    transform(arr: any[]){
+    transform(arr: any[] ){
         return arr.reverse();
     }
 }
