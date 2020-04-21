932cb1cb37f0aca2db927981578c900d3ef5b416
kattisravni24 <kattisravni24@gmail.com>
2020-04-21 09:46:31 +0530

    latest commits

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,4 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent implements OnInit{
     constructor(){ }
     ngOnInit() {}
+    inputClicked(firstName){
+        console.log(firstName);
+    }
 }  
